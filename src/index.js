9018072da274fa23cc2afb8ae4066f8abe774a55
Luis David <luischinchilla12364@gmail.com>
2022-10-19 16:25:58 -0400

    users

--- src/index.js
+++ src/index.js
@@ -14,4 +14,4 @@ app.use('/', usersRouter)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log('Success 😺😺😺 ' + PORT))
+app.listen(PORT, () => console.log('Success' + PORT))
--- src/users/users.controllers.js
+++ src/users/users.controllers.js
@@ -3,25 +3,25 @@ const uuid = require('uuid')
 const usersDB = [
     {
         id: '6438461d-b35a-4481-98c4-e0550feb3d2b',
-        first_name: 'Cristina',
-        last_name: 'Gómez',
-        email: 'maria.c@gmail.com',
-        password: 'cistina2000',
-        birthday: '2000/10/04'
+        first_name: 'Luis David',
+        last_name: 'Fernandez',
+        email: 'ldfernandez@gmail.com',
+        password: 'David555',
+        birthday: '1998/11/22'
     },
     {
         id: '45b1ebbd-ca98-4b53-8585-74430b25cfa4',
-        first_name: 'Erika',
-        last_name: 'López',
-        email: 'erika.lopez@gmail.com',
-        password: 'erika1234',
-        birthday: '2000/02/10'
+        first_name: 'Lucas',
+        last_name: 'Prato',
+        email: 'lucas.prato@gmail.com',
+        password: 'lucas1234',
+        birthday: '2001/05/18'
     },
     {
         id: 'e3566835-17aa-4843-9c14-7df45317f09c',
-        first_name: 'Javier',
-        last_name: 'C',
-        email: 'venerablepseudo.11@gmail.com',
+        first_name: 'Poncio',
+        last_name: 'Costa',
+        email: 'pocio.costas@gmail.com',
         password: '5683',
         birthday: '2000/11/28'
     },
@@ -48,4 +48,4 @@ const createUser = (data) => {
     return newUser
 }
 
-module.exports = { getAllUsers, getUsersById, createUser }
+module.exports = { getAllUsers, getUsersById, createUser}
